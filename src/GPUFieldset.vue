3aa3ed7a0cc54fe8c79ee9a99acaf3ff50d5079b
Justaphf <juholmes@vt.edu>
2024-12-01 13:06:39 -0500

    WIP: Display some nVidia GPU measurements
    1. Requires feature in CBang (required by fah-client-bastet)
    2. Requires feature in fah-client-bastet

--- src/GPUFieldset.vue
+++ src/GPUFieldset.vue
@@ -67,6 +67,33 @@ fieldset.view-panel.gpu-fieldset
     info-item(label="PCI Device ID", :content="'0x' + gpu.device.toString(16)")
     info-item(label="PCI Vendor ID", :content="'0x' + gpu.vendor.toString(16)")
 
+  .info-group(v-if="gpu.nvml")
+    info-item(label="PCIe Link (Max)", :content="gpu.pcie_link_system + '(' + gpu.pcie_link_device + ')'")
+    info-item(label="PCIe Link (Current)", :content="gpu.pcie_link_system")
+
+  .info-group(v-if="gpu.nvml")
+    info-item(label="GPU Clock (MHz)", :content="gpu.cur_gpu_clock.toString(10)+'/'+gpu.max_gpu_clock.toString(10)")
+    info-item(label="Mem Clock (MHz)", :content="gpu.max_mem_clock.toString(10)+'/'+gpu.max_mem_clock.toString(10)")
+
+  .info-group(v-if="gpu.nvml")
+    info-item(label="PState", :content="gpu.pstate")
+    info-item(label="GPU Temp (C)", :content="gpu.cur_temp")
+
+  .info-group(v-if="gpu.nvml")
+    info-item(label="Power (Current)", :content="gpu.cur_gpu_power")
+    info-item(label="Power (Limit)", :content="gpu.max_gpu_power")
+
+  .info-group(v-if="gpu.nvml")
+    info-item(label="Fans", :content="gpu.gpu_fans")
+    info-item(v-if="gpu.gpu_fans > 0", label="Fan (%)", :content="gpu.cur_fan_pct")
+
+  .info-group(v-if="gpu.nvml && gpu.gpu_fans > 0")
+    info-item(label="Fan 1 (%)", :content="gpu.fan0_pct")
+    info-item(label="Fan 2 (%)", :content="gpu.fan1_pct")
+
+  .info-group(v-if="gpu.nvml && gpu_fans > 2")
+    info-item(label="Fan 3 (%)", :content="gpu.fan2_pct")
+
   .info-group(v-for="dev of devs")
     info-item(:label="dev.name",
       :content="(dev.supported ? '' : 'un') + 'supported'")
